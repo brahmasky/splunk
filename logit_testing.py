0b44dfd8c434276d33ebde4a420b9fbb6db8bfe5
Paco Luo <51429440+brahmasky@users.noreply.github.com>
2019-06-29 22:18:03 +1000

    Update logit_testing.py

--- logit_testing.py
+++ logit_testing.py
@@ -334,7 +334,7 @@ def backup_splunk(splunk_home, backup_dir):
 
 def yes_or_no(question):
     while "the answer is invalid":
-        reply = str(input(question+' (y/n): ')).lower().strip()
+        reply = str(raw_input(question+' (y/n): ')).lower().strip()
         if reply[:1] == 'y':
             return True
         if reply[:1] == 'n':
